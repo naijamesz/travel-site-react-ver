f84fdfae285969ce9e17cb3fea58c846f06474b9
naijamesz <kodlikejamesz@hotmail.com>
2023-09-03 06:24:20 +0700

    first commit

---
+++ app/assets/scripts/modules/MyAmazingComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function MyAmazingComponent() {
+  return (
+    <>
+      <div className='testimonial__title'>
+        <h1 className='section-title section-title--blue'>This Is My Amazing React Component</h1>
+        <p>React is great!!!</p>
+      </div>
+    </>
+  );
+}
+
+export default MyAmazingComponent;
